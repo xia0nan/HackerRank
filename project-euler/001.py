27df94803bf2d69249263813e4c0eb6c7ab4b9c3
Nan <xnone0104@gmail.com>
2019-04-12 16:20:41 +0800

    add problem 1

---
+++ project-euler/001.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys
+
+def sum(n, k):
+    d = n // k
+    return k * (d * (d+1)) // 2
+
+def euler1(n):
+    return sum(n, 3) + sum(n, 5) - sum(n, 15)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(euler1(n-1))
+
+### https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem
